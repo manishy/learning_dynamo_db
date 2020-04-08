8cca9a419cd40da7b9a4a14d2e92fc706cc1c782
Manish <manishy@thoughtworks.com>
2020-04-08 14:42:36 +0530

    Refactor loadSampleData

--- loadSampleData.js
+++ loadSampleData.js
@@ -1,38 +1,41 @@
-var AWS = require("aws-sdk");
-var fs = require('fs');
+const AWS = require("aws-sdk");
+const fs = require('fs');
 
 AWS.config.update({
     region: "us-west-2",
     endpoint: "http://localhost:8000"
 });
 
-var docClient = new AWS.DynamoDB.DocumentClient();
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const put = (params)=>{
+    docClient.put(params, function(err, info) {
+        if (err) {
+            console.error("Unable to add movie", data.title, ". Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("PutItem succeeded:", data.title);
+        }
+    });
+};
 
 const load = (data)=>{
-    var params = {
+    const params = {
         TableName: "Movies",
         Item: {
-            "year":  data.year,
+            "year": data.year,
             "title": data.title,
-            "info":  data.info
+            "info": data.info
         }
     };
+    put(params);
+};
 
-    docClient.put(params, function(err, info) {
-       if (err) {
-           console.error("Unable to add movie", data.title, ". Error JSON:", JSON.stringify(err, null, 2));
-       } else {
-           console.log("PutItem succeeded:", data.title);
-       }
-    });
-}
-
-const loadData = ()=>{
+const loadDummyData = ()=>{
 console.log("Importing movies into DynamoDB. Please wait.");
-    var allMovies = JSON.parse(fs.readFileSync('moviedata.json', 'utf8'));
+    const allMovies = JSON.parse(fs.readFileSync('moviedata.json', 'utf8'));
     console.log(allMovies);
     allMovies.forEach(load);
-}
+};
 
-loadData();
+loadDummyData();
 
