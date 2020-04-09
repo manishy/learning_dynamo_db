818c1189fa33832b4040e4bb0e2f916ced017db8
Manish <manishy@thoughtworks.com>
2020-04-09 14:20:35 +0530

    Add conditional query to fetch data

---
+++ documentClient/conditionalQuery.js
@@ -0,0 +1,37 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "us-west-2",
+    endpoint: "http://localhost:8000"
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const queryMovies =(params)=>{
+    console.log(`Querying for movies from ${params.ExpressionAttributeValues[":yyyy"]}.`);
+    docClient.query(params, function(err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Query succeeded.");
+            data.Items.forEach(function(item) {
+                console.log(" -", item.year + ": " + item.title);
+            });
+        }
+    });
+};
+
+const params = {
+    TableName: "Movies",
+    KeyConditionExpression: "#yr = :yyyy",
+    ExpressionAttributeNames: {
+        "#yr": "year"
+    },
+    ExpressionAttributeValues: {
+        ":yyyy": 2015
+    }
+};
+
+queryMovies(params);
+
+
